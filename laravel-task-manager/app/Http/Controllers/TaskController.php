d4deb0c2d4de97d5c5f1656cb004a5b6f0fdd9d3
Laizy7 <ehrenvisteano7@gmail.com>
2023-10-28 22:44:55 +0700

    Progress 1:32:49

--- laravel-task-manager/app/Http/Controllers/TaskController.php
+++ laravel-task-manager/app/Http/Controllers/TaskController.php
@@ -9,44 +9,45 @@ use Spatie\QueryBuilder\QueryBuilder;
 use App\Http\Resources\TaskCollection;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function index(Request $request)
-    {
-        $tasks = QueryBuilder::for(Task::class)
-            ->allowedFilters('is_done')
-            ->defaultSort('-created_at')
-            ->allowedSorts(['title', 'is_done', 'created_at'])
-            ->paginate();
+  public function index(Request $request)
+  {
+    $tasks = QueryBuilder::for(Task::class)
+      ->allowedFilters('is_done')
+      ->defaultSort('-created_at')
+      ->allowedSorts(['title', 'is_done', 'created_at'])
+      ->paginate();
 
-        return new TaskCollection($tasks);
-    }
+    return new TaskCollection($tasks);
+  }
 
-    public function show(Request $request, Task $task)
-    {
-        return new TaskResource($task);
-    }
+  public function show(Request $request, Task $task)
+  {
+    return new TaskResource($task);
+  }
 
-    public function store(StoreTaskRequest $request)
-    {
-        $validated = $request->validated();
+  public function store(StoreTaskRequest $request)
+  {
+    $validated = $request->validated();
 
-        $task = Task::create($validated);
-        return new TaskResource($task);
-    }
+    $task = Auth::user()->tasks()->create($validated);
+    return new TaskResource($task);
+  }
 
-    public function update(UpdateTaskRequest $request, Task $task)
-    {
-        $validated = $request->validated();
+  public function update(UpdateTaskRequest $request, Task $task)
+  {
+    $validated = $request->validated();
 
-        $task->update($validated);
-        return new TaskResource($task);
-    }
+    $task->update($validated);
+    return new TaskResource($task);
+  }
 
-    public function destroy(Request $request, Task $task)
-    {
-        $task->delete();
-        return response()->noContent();
-    }
+  public function destroy(Request $request, Task $task)
+  {
+    $task->delete();
+    return response()->noContent();
+  }
 }
